13fd42fee43eb638d26ca40a9aa2843ff7931fdd
福田　淳 <clickmanver1.01@gmail.com>
2016-11-08 13:45:46 +0900

    PinBall　プロジェクト　得点を表示できるようにした。
    得点を表示するスクリプトScore.csを作成。得点の計算はBallController.csの31行目以降に追記。

---
+++ Assets/BallController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    public Score score;     //得点を表示するためのスクリプト「Score」を参照
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        this.gameoverText = GameObject.Find("GameOverText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+    }
+
+    //スコアを加算するための計算
+    void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.tag == "SmallStarTag")
+        {
+            score.GetComponent<Score>().scorePt += 5;
+        }
+        else if (target.gameObject.tag == "LargeStarTag")
+        {
+            score.GetComponent<Score>().scorePt += 15;
+        }
+        else if (target.gameObject.tag == "SmallCloudTag")
+        {
+            score.GetComponent<Score>().scorePt += 10;
+        }
+        else if (target.gameObject.tag == "LargeCloudTag")
+        {
+            score.GetComponent<Score>().scorePt += 20;
+        }
+
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private GameObject scoreText;
+    public int scorePt = 0;   
+
+    // Use this for initialization
+    void Start () {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.scoreText.GetComponent<Text>().text = "Score " + scorePt.ToString();
+    }
+}
