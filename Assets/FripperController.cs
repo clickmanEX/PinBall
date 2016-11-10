75a3a958e351504dff31e8ca90b6e50b4ac95cf9
福田　淳 <clickmanver1.01@gmail.com>
2016-11-10 21:49:30 +0900

    PinBall　プロジェクト　得点表示、マルチタッチに対応
    GameSceneの参照が誤っていたので修正。得点表示はScore.cs、得点計算はBallController.csの31行目以降に追記。
    発展課題のマルチタッチはFripperController.cs38～65行目に追記。

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -6,6 +6,7 @@ public class FripperController : MonoBehaviour {
     private HingeJoint myHingeJoynt;
     private float defaultAngle = 20;
     private float flickAngle = -20;
+    
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +15,10 @@ public class FripperController : MonoBehaviour {
         SetAngle(this.defaultAngle);
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
         {
@@ -32,7 +34,37 @@ public class FripperController : MonoBehaviour {
         {
             SetAngle(this.defaultAngle);
         }
+
+        ///タッチ座標をスクリーン座標からワールド座標に変換し、x座標で画面を左右2分割にしてフリッパーを制御
+        foreach (Touch touch in Input.touches)
+        {
+            ///タッチ座標がx,y座標しかないのでz座標を追加し、ワールド座標に変換
+            Vector3 pos = touch.position;
+            pos.z = 100f;
+
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(pos);
+
+
+            if (touch.phase == TouchPhase.Began && worldPos.x <= 0 && tag == "LeftFripperTag")
+            {
+                SetAngle(this.flickAngle);
+                Debug.Log(worldPos.x);
+            }
+
+            if (touch.phase == TouchPhase.Began && worldPos.x > 0 && tag == "RightFripperTag")
+            {
+                SetAngle(this.flickAngle);
+                Debug.Log(worldPos.x);
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                SetAngle(this.defaultAngle);
+            }
+
+        }
     }
+
 
     public void SetAngle(float angle)
     {
--- Assets/Score.cs
+++ Assets/Score.cs
@@ -4,17 +4,16 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
 
-    private GameObject scoreText;
     public int scorePt = 0;   
 
     // Use this for initialization
     void Start () {
-        this.scoreText = GameObject.Find("ScoreText");
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        this.scoreText.GetComponent<Text>().text = "Score " + scorePt.ToString();
+        this.GetComponent<Text>().text = "Score " + scorePt.ToString();
     }
 }
